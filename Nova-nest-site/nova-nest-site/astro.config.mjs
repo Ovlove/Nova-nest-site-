71d9daeffd4b44b215194e8ffeb8d2f10ea012cc
Ovlove <ovwemue@gmail.com>
2025-06-14 21:46:54 +0100

    Update astro.config.mjs

--- Nova-nest-site/nova-nest-site/astro.config.mjs
+++ Nova-nest-site/nova-nest-site/astro.config.mjs
@@ -3,8 +3,8 @@ import vercel from "@astrojs/vercel/serverless";
 import image from "@astrojs/image";
 
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
-  integrations: [image()],
-  site: "https://nova-nest.vercel.app",
+  output: "server",                     // Required for serverless deployment
+  adapter: vercel(),                   // Astro will use Vercel adapter
+  integrations: [image()],             // Enables <Image /> optimization
+  site: "https://nova-nest.vercel.app" // Sets your site base URL
 });
