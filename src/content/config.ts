2aa187ed14c32a9e4589bdca77fc6b868ae0f655
Ovlove <ovwemue@gmail.com>
2025-06-15 17:36:13 +0100

    Create config.ts

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+// src/content/config.ts
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+  }),
+});
+
+export const collections = {
+  blog,
+};
