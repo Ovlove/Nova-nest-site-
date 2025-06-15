1eddf445ab97cdae08d274ad610a9cf6f361dc83
Ovlove <ovwemue@gmail.com>
2025-06-15 23:20:25 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  output: 'static',
-  integrations: [tailwind()],
+  output: 'static', // Use 'server' if you need SSR
+  adapter: vercel(),
+  integrations: [
+    tailwind(),
+    mdx()
+  ]
 });
