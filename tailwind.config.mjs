a5697a49ae4a9e919fa0d471260dd4ac91a0282d
Ovlove <ovwemue@gmail.com>
2025-06-15 23:53:59 +0100

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,7 @@ import typography from '@tailwindcss/typography';
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}'],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: [typography]
+  plugins: [typography],
 };
