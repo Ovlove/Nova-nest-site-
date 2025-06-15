6d10e3bf851a55c2b7fe1514e81e3b3257db3a3a
Ovlove <ovwemue@gmail.com>
2025-06-15 22:30:50 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
+  output: 'static',
   integrations: [tailwind()],
 });
