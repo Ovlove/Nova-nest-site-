8656155e0f7e5dbb0761c3137184ba9a17226f63
Ovlove <ovwemue@gmail.com>
2025-06-16 01:24:27 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  integrations: [
-    tailwind({
-      configFile: './tailwind.config.mjs',
-    }),
-    mdx(),
-    vercel(),
-  ],
-  output: 'server', // needed for MDX dynamic routes
+  output: 'static',
+  adapter: vercel(),
+  integrations: [tailwind(), mdx()],
 });
