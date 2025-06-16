57d9550f627f41f33c400b32f35e35e5f1264e49
Ovlove <ovwemue@gmail.com>
2025-06-16 06:18:31 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless'; // Use `/serverless` for dynamic routes
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
